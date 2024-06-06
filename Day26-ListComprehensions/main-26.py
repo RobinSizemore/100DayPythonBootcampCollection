cb1b0a1d14f82ab89c145dc9ed4a1a7d2819fb4a
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-06-06 16:29:21 -0400

    Days 26 and 27 completed.

---
+++ Day26-ListComprehensions/main-26.py
@@ -0,0 +1,10 @@
+# Comprehension Understanding Exercise
+import pandas
+
+nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}  # pandas df to dictionary using comprehension
+entered_word = ""
+while not entered_word == 'QUIT':
+    entered_word = input("Please enter the word to transcribe: ").upper()
+    list_output = [nato_dict[letter] for letter in entered_word]  # word to list using dict and comprehension
+    print(list_output)
